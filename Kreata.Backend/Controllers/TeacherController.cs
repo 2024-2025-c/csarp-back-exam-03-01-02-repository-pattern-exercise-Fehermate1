d7e0381406364d75adf2bac2e00ed3ffbc0aefe2
Máté Fehér <c9fehmat@vasvari.org>
2024-12-17 11:32:36 +0100

    teachercontroller, repo felvetele services koze type shhhhh

---
+++ Kreata.Backend/Controllers/TeacherController.cs
@@ -0,0 +1,24 @@
+﻿using Kreata.Backend.Repos;
+using Kreta.Shared.Dtos;
+using Kreta.Shared.Extensions;
+using Kreta.Shared.Models;
+using Kreta.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kreata.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeacherController : ControllerBase
+    {
+        private ITeacherRepo? _teacherRepo;
+        public TeacherController(ITeacherRepo? teacherRepo)
+        {
+            _teacherRepo = teacherRepo ?? throw new ArgumentNullException(nameof(teacherRepo));
+        }
+        public TeacherController()
+        {
+            
+        }
+    }
+}
--- Kreata.Backend/Repos/TeacherRepo.cs
+++ Kreata.Backend/Repos/TeacherRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace Kreata.Backend.Repos
+﻿using Kreata.Backend.Context;
+using Kreata.Backend.Repos.Base;
+using Kreta.Shared.Models;
+
+namespace Kreata.Backend.Repos
 {
-    public class TeacherRepo
+    public class TeacherRepo<TDbContext> : BaseRepo<TDbContext, Teacher>, ITeacherRepo where TDbContext : KretaContext
     {
+        public TeacherRepo(TDbContext? dbContext) : base(dbContext) 
+        {
+            
+        }
     }
 }
